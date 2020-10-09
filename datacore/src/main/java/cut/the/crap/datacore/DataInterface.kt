6acc5f455403c2be96cc26e637a249875eecc459
Marco Leiter <marco.leiter@external.sinnerschrader.com>
2020-10-09 13:14:59 +0200

    Setting up a acceptable to clone state

---
+++ datacore/src/main/java/cut/the/crap/datacore/DataInterface.kt
@@ -0,0 +1,13 @@
+package cut.the.crap.datacore
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+interface DataInterface  {
+
+
+
+}
